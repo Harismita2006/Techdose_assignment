ad2e552017a27be9c9f08c4ac452071c9a2e2f98
Harismita2006 <152049393+Harismita2006@users.noreply.github.com>
2025-02-07 09:32:07 +0530

    Create leetcode_169.cpp

---
+++ Day3/leetcode_169.cpp
@@ -0,0 +1,19 @@
+//Majority Element
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = 0;
+        for ( int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+            int bits = 0;
+            for (int num : nums) {
+                if (num & mask) {
+                    bits++;
+                }
+            }
+            if (bits > nums.size() / 2) {
+                majority |= mask;
+            }
+        }
+        return majority;
+    }
+};
